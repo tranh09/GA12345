318a63903e2290ae4dd6144cb5f4f026edbd6194
trung anh <nguyendotrunganh19092004@gmail.com>
2025-01-10 20:56:59 +0700

    viết bài

---
+++ ConsoleApplication7/ConsoleApplication7/ConsoleApplication7.cpp
@@ -0,0 +1,28 @@
+﻿// ConsoleApplication7.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
+	// char: kiểu dữ liệu ký tự ,có kích thước 1 byte
+	char x = 'X';                                                                                                                                                                                       
+	cout << a  << endl;
+
+	// sizeof : size of
+	cout << "kich thuoc cua du lieu char = " << sizeof(a) << "byte" << endl;
+	//string: kiểu dữ liệu chuỗi ký tự 
+	string name = " Nguyen Do Trung Anh";
+	
+	//số nguyên
+	
+	//int: integer, có kích thước 4 byte
+	int number = 100;
+	//long : có kích thước 8 byte
+	long largeNumber = 100000000
+
+
+}
+
