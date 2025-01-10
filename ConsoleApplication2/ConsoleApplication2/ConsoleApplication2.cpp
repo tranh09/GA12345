6e07ae0fd0053cf64aa86cd0168405322a2927b8
trung anh <nguyendotrunganh19092004@gmail.com>
2025-01-10 19:00:14 +0700

    bài 1

--- ConsoleApplication2/ConsoleApplication2/ConsoleApplication2.cpp
+++ ConsoleApplication2/ConsoleApplication2/ConsoleApplication2.cpp
@@ -1,4 +1,4 @@
-// ConsoleApplication1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// ConsoleApplication2.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
 #include <iostream>
